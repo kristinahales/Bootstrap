7187010fd1f506781da62bf48482e05d55fd8a5f
Krissy Hales <kristinakfriel@gmail.com>
2019-07-15 19:11:37 -0600

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,31 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                <div className='small-container'>
-                    <p>Services</p>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <div className='container1'>
+                    <span className='text1'>Services</span>
+                    <span className='text2'>Lorem ipsum dolor sit amet consectetur.</span>
+                </div>
+
+                <div className='container2'>
+                    <div className='container3'>
+                        <i class="fas fa-shopping-cart"></i>
+                        <h4> E-Commerce</h4>
+                        <p className='text3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+
+                    <div className='container3'>
+                        <i class="fas fa-tv"></i>
+                        <h4>Responsive Design</h4>
+                        <p className='text3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+
+                    
+                    <div className='container3'>
+                        <i class="fas fa-lock"></i>
+                        <h4>Web Security</h4>
+                        <p className='text3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+                    </div>
+
                 </div>
             </div>
         )
